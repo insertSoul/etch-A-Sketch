ebcb3b28998ea3b599d89bbc0f8da4351771001c
insertSoul <j.clarke213@gmail.com>
2022-12-02 19:05:53 +0000

    add pseudo code

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// ############# Etch-a-sketch pseudo code #########
+
+// Creating the grid of divs
+    // make a single sized square to replicate in css
+// Container to hold grid in -- might need to make as a flexbox to contain girdelements
+    // repeat 16 across (row)
+        // repeat that row down 16 (loop in loop) (column)
+            //(make 16 a changeable variable to make custom grid?)
+            // would need to adjust size of squares so it would fit in total space
+    
+
+
+// querySelectAll girdSquares
+// onHover change attribute of background color (addEventListener('hoover'))
+    //Could add to color value to give grayscale addition where multiple movements across grid would darken 
+
+// Need to create a way for clearing the grid
+// this is where we could implement the ability to change grid size
